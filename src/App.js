f85f3baab60e7b6d53d8d30eaa8d3e769f550f98
pavanimyana123 <pavanimyana2000@gmail.com>
2024-11-25 16:44:20 +0530

    Pavani

--- src/App.js
+++ src/App.js
@@ -7,26 +7,16 @@ import {
   Routes,
 } from "react-router-dom";
 import './App.css';
-
-
-import Repairs from "./Components/Modules/Transactions/Repairs/Repairs"; // Adjust the path based on your project structure
-
-
+import Repairs from "./Components/Modules/Transactions/Repairs/Repairs"; 
 import ItemMaster from "./Components/Pages/ItemMasters/ItemMaster";
 
 function App() {
   return (
     <Router>
-
-    
       <Routes>
         <Route path="/itemmaster" exact element={<ItemMaster />} />
-
-     
-        {/* Other Routes */}
         <Route path="/repairs" element={<Repairs />} />
       </Routes>
-
     </Router>
   );
 }
