d5ef05299aa7566a32450df5e7478b656df0952b
sai <asaikrishnachary103@gmail.com>
2024-12-24 12:11:13 +0530

    sai

--- src/Components/Modules/Reports/PurchaseReport/PurchaseReport.js
+++ src/Components/Modules/Reports/PurchaseReport/PurchaseReport.js
@@ -1,225 +1,136 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import DataTable from '../../../Pages/InputField/DataTable'; // Import the reusable DataTable component
+import DataTable from '../../../Pages/InputField/TableLayout'; // Import the reusable DataTable component
 import { FaEdit, FaTrash } from 'react-icons/fa';
-import { Button, Row, Col, Form  } from 'react-bootstrap';
+import { Button, Row, Col } from 'react-bootstrap';
+
+import baseURL from "../../../../Url/NodeBaseURL";
 
 const PurchaseTable = () => {
   const navigate = useNavigate();
   const [data, setData] = useState([]); // State to store table data
 
-
+  const formatDate = (date) => {
+    if (!date) return ''; // Return an empty string if no date is provided
+  
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2, '0');  // Pad single digit days with 0
+    const month = String(d.getMonth() + 1).padStart(2, '0'); // Pad months with 0
+    const year = d.getFullYear();
+    
+    return `${day}/${month}/${year}`;
+  };
 
   const columns = React.useMemo(
     () => [
-      {
-        Header: 'Name',
-        accessor: 'supplier_id',
-      },
-        
-      {
-        Header: 'Metal',
-        accessor: 'metal',
-      },
-      {
-        Header: 'Purity',
-        accessor: 'purity',
-      },
-      {
-        Header: 'A/C Address:',
-        accessor: 'a/c_address',
-      },
-      {
-        Header: 'GSTIN',
-        accessor: 'gst_in',
-      },
-      {
-        Header: ' Indent',
-        accessor: 'indent',
-      },
-      {
-        Header: 'Bill No:',
-        accessor: 'bill_no',
-      },
-      {
-        Header: 'Type',
-        accessor: 'type',
-      },
-      {
-        Header: 'Rate_Cut',
-        accessor: 'rate_cut',
-      },
-      {
-        Header: 'Date',
-        accessor: 'date',
-      },
-      {
-        Header: 'Bill Date:',
-        accessor: 'bill_date',
-      },
-      {
-        Header: 'Category',
-        accessor: 'category',
-      },
-      {
-        Header: 'Due Date',
-        accessor: 'due_date',
-      },
-      {
-        Header: 'Rate',
-        accessor: 'rate',
-      },
-      {
-        Header: 'Item Name',
-        accessor: 'item_name',
-      },
-      {
-        Header: 'Design Code ',
-        accessor: 'design_code',
-      },
-      {
-        Header: ' HSN',
-        accessor: 'hsn',
-      },
-      // {
-      //   Header: 'Type',
-      //   accessor: 'type',
-      // },
-      {
-        Header: 'Stock Type',
-        accessor: 'stock_type',
-      },
-      // {
-      //   Header: 'Pcs',
-      //   accessor: 'pcs',
-      // },
-      {
-        Header: 'Gross',
-        accessor: 'gross',
-      },
-      {
-        Header: 'Stone',
-        accessor: 'stone',
-      },
-      {
-        Header: 'Net',
-        accessor: 'net',
-      },
-      // {
-      //   Header: 'Purity',
-      //   accessor: 'purity',
-      // },
-      // {
-      //   Header: 'Rate',
-      //   accessor: 'rate',
-      // },
-      {
-        Header: 'Unit',
-        accessor: 'unit',
-      },
-      {
-        Header: 'W% ',
-        accessor: 'w%',
-      },
-      {
-        Header: 'Waste ',
-        accessor: 'waste',
-      },
-      {
-        Header: 'Pure Wt',
-        accessor: 'pure_wt',
-      },
-      {
-        Header: 'Alloy ',
-        accessor: 'alloy',
-      },
-      {
-        Header: 'Cost',
-        accessor: 'cost',
-      },
-      {
-        Header: 'Tot Wt :',
-        accessor: 'total_wt',
-      },
-      {
-        Header: 'WT*Rate Amt',
-        accessor: 'wt_rate_amt',
-      },
-      {
-        Header: 'MC/GM',
-        accessor: 'mc/gm',
-      },
-      {
-        Header: 'MC',
-        accessor: 'mc',
-      },
-      {
-        Header: 'Stn. Amt',
-        accessor: 'stn_amt',
-      },
-      {
-        Header: 'Total ',
-        accessor: 'total',
-      },
-      // {
-      //   Header: 'Stone ',
-      //   accessor: 'stone',
-      // },
-      {
-        Header: 'Pcs ',
-        accessor: 'pcs',
-      },
-      {
-        Header: 'CT ',
-        accessor: 'ct',
-      },
-
-      {
-        Header: 'Gms  ',
-        accessor: 'gms',
-      },
-      {
-        Header: 'CWP ',
-        accessor: 'cwp',
-      },
-    
-      // {
-      //   Header: 'Rate  ',
-      //   accessor: 'rate',
-      // },
-      {
-        Header: 'Clear  ',
-        accessor: 'clear',
-      },
-      {
-        Header: 'Class  ',
-        accessor: 'class',
-      },
-      {
-        Header: 'Cut  ',
-        accessor: 'cut',
-      },
-      {
-        Header: 'Clarity  ',
-        accessor: 'clarity',
-      },
-     
-      {
-        Header: 'Action',
-       
-          
-      },
-      
+      { Header: 'Name', accessor: 'account_name' },
+      { Header: 'Mobile', accessor: 'mobile' },
+      { Header: 'Email', accessor: 'email' },
+      // { Header: 'A/C Address:', accessor: 'address1' },
+      // { Header: 'Address 2', accessor: 'address2' },
+      // { Header: 'City', accessor: 'city' },
+      // { Header: 'Pincode', accessor: 'pincode' },
+      // { Header: 'State', accessor: 'state' },
+      // { Header: 'State Code', accessor: 'state_code' },
+      // { Header: 'Aadhar Card', accessor: 'aadhar_card' },
+      // { Header: 'GSTIN', accessor: 'gst_in' },
+      // { Header: 'PAN Card', accessor: 'pan_card' },
+      // { Header: 'Indent', accessor: 'indent' },
+      // { Header: 'Bill No:', accessor: 'bill_no' },
+      // { Header: 'Type', accessor: 'type' },
+      // { Header: 'Rate Cut', accessor: 'rate_cut' },
+      { Header: 'Date', accessor: 'date',Cell: ({ value }) => <span>{formatDate(value)}</span>, },
+      { Header: 'Bill Date:', accessor: 'bill_date',Cell: ({ value }) => <span>{formatDate(value)}</span>, },
+      { Header: 'Due Date', accessor: 'due_date',Cell: ({ value }) => <span>{formatDate(value)}</span>, },
+      // { Header: 'Purchase Rate', accessor: 'Purchase_rate' },
+      // { Header: 'Product ID', accessor: 'product_id' },
+      { Header: 'Product Name', accessor: 'product_name' },
+      { Header: 'Metal Type', accessor: 'metal_type' },
+      { Header: 'Design Name', accessor: 'design_name' },
+      { Header: 'Purity', accessor: 'purity' },
+      // { Header: 'HSN', accessor: 'hsn' },
+      // { Header: 'Product Type', accessor: 'product_type' },
+      // { Header: 'Stock Type', accessor: 'stock_type' },
+      // { Header: 'Pcs', accessor: 'pcs' },
+      { Header: 'Gross Weight', accessor: 'gross_weight' },
+      { Header: 'Stone Weight', accessor: 'stone_weight' },
+      // { Header: 'Net Weight', accessor: 'net_weight' },
+      // { Header: 'Nit Weight', accessor: 'nit_weight' },
+      // { Header: 'Waste Percentage', accessor: 'waste_percentage' },
+      // { Header: 'Waste Amount', accessor: 'waste_amount' },
+      // { Header: 'Pure Weight', accessor: 'pure_weight' },
+      // { Header: 'Alloy', accessor: 'alloy' },
+      // { Header: 'Cost', accessor: 'cost' },
+      // { Header: 'Total Weight', accessor: 'total_weight' },
+      // { Header: 'WT*Rate Amount', accessor: 'wt_rate_amount' },
+      // { Header: 'MC/GM', accessor: 'mc_per_gram' },
+      // { Header: 'MC', accessor: 'mc' },
+      // { Header: 'Stone Amount', accessor: 'stone_amount' },
+      // { Header: 'Total Amount', accessor: 'total_amount' },
+      // { Header: 'Stone', accessor: 'stone' },
+      // { Header: 'Stone Pcs', accessor: 'stone_pcs' },
+      // { Header: 'Stone CT', accessor: 'stone_ct' },
+      // { Header: 'CWP', accessor: 'cwp' },
+      // { Header: 'GMS', accessor: 'gms' },
+      // { Header: 'Stone Rate', accessor: 'stone_rate' },
+      // { Header: 'Clarity', accessor: 'clarity' },
+      // { Header: 'Rate', accessor: 'rate' },
+      // { Header: 'Clear', accessor: 'clear' },
+      // { Header: 'Class', accessor: 'class' },
+      // { Header: 'Cut', accessor: 'cut' },
+      // { Header: 'Action', accessor: 'action' },
     ],
     []
   );
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${baseURL}/get/purchases`); // Fetch data from the endpoint
+        const result = await response.json();
+        if (result?.purchases) {
+          setData(result.purchases); // Set fetched data
+        }
+      } catch (error) {
+        console.error('Error fetching payments:', error);
+      }
+    };
+    fetchData();
+  }, []);
+
+    // Fetch data from the API
+    // useEffect(() => {
+    //   const fetchData = async () => {
+    //     try {
+    //       const response = await fetch(`${baseURL}/get/purchases`);
+    //       if (!response.ok) {
+    //         throw new Error(`HTTP error! status: ${response.status}`);
+    //       }
+    //       const result = await response.json();
+    //       setData(Array.isArray(result) ? result : [result]); // Ensure the result is an array
+    //     } catch (error) {
+    //       console.error('Error fetching data:', error);
+    //     } finally {
+    //       // setLoading(false); // Stop loading spinner
+    //     }
+    //   };
+  
+    //   fetchData();
+    // }, []);
+
+  const handleCreate = () => {
+    navigate('/purchase'); // Navigate to the /suppliers page
+  };
 
   return (
     <div className="main-container">
-      <div className="sales-table-container">
+      <div className="purchase-table-container">
         <Row className="mb-3">
           <Col className="d-flex justify-content-between align-items-center">
-            <h3>Purchase Report</h3>          
+            <h3>Purchase</h3>
+            {/* <Button  className='create_but' onClick={handleCreate} style={{ backgroundColor: '#a36e29', borderColor: '#a36e29' }}>
+              + Create
+            </Button> */}
           </Col>
         </Row>
         <DataTable columns={columns} data={data} />
